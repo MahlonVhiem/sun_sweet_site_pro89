32fa8e12ded2ece668dfa0e39be5cfaec2442173
mahlon <yassim@gmail.com>
2025-12-30 01:43:32 -0400

    feat: update hotel address and google maps link

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig = {
     phone: "+597-868-9487",
     email: "sunsmilehotel68@gmail.com",
     whatsapp: "5978689487", 
-    address: "Jacobastraat #15, Paramaribo, Suriname",
-    googleMapsLink: "https://maps.app.goo.gl/isyxydReJLqVDU5G9?g_st=awb",
+    address: "Jacabastraat 15, Paramaribo, Suriname",
+    googleMapsLink: "https://www.google.com/maps/dir/Restaurant+Ondrobong,+RRGM%2BQMJ,+Paramaribo,+Suriname/Smile+Hotel+%26+Appartementen,+Jacabastraat+15,+Paramaribo,+Suriname/@5.8393498,-55.1869418,18z/data=!4m13!4m12!1m5!1m1!1s0x8d09cacea6274479:0x1785855c214a95ce!2m2!1d-55.1658606!2d5.826996!1m5!1m1!1s0x8d0834d6dc7553fb:0x36c0174b3a06da9f!2m2!1d-55.1862408!2d5.839308?hl=en-US&entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoKLDEwMDc5MjA2OUgBUAM%3D",
   },
 };
